4a6e6ffba792f70de81be5f24db8367ed77bb6e8
aiden-patterson <pat18001@byui.edu>
2024-02-02 12:50:22 -0700

    worked on Decorator

--- 331.Skeet/impulse.cpp
+++ 331.Skeet/impulse.cpp
@@ -1,20 +1,26 @@
 #include "impulse.h"
 void ApplyInertia::impulse(Bird* bird) {
 	Velocity v = bird->getVelocity();
-	bird->getPosition().add(v);
+	Position p = bird->getPosition();
+	p.add(v);
+	*bird = p;
 }
 
 void ApplyGravity::impulse(Bird* bird) {
 	Velocity v = bird->getVelocity();
 	v.addDy(-0.04);
+	*bird = v;
 }
 
 void ApplyBouyancy::impulse(Bird* bird) {
-
+Velocity v = bird->getVelocity();
+v.addDy(0.05);
+*bird = v;
 }
 
 void ApplyDrag::impulse(Bird* bird) {
-
+	Velocity v = bird->getVelocity();
+	
 }
 
 void ApplyTurn::impulse(Bird* bird) {
